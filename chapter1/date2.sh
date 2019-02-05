0b9c630ed8e9966f7de937e172963824bdee1629
skyele <skyele@sjtu.edu.cn>
2019-02-05 22:17:00 +0800

    finish date

--- chapter1/date2.sh
+++ chapter1/date2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 date --date "Jan 20 2010" +%A
+date "+%d %B %Y"
---
+++ chapter1/time_take.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+start=$(date +%s)
+end=$(date +%s)
+difference=$(( end - start))
+echo Time taken to execute commands is $difference seconds.
